f6226f9db0c528622a2d6cae7c8d68915ad49c28
Jane <lvlajzhao1983@gamil.com>
2023-05-23 16:18:22 +0800

    remove unused codes

--- MiddlewareDemo/gin-Recovery.go
+++ MiddlewareDemo/gin-Recovery.go
@@ -10,7 +10,6 @@ import (
 func GinRecoveryDemo() {
 	r := gin.New()
 	r.Use(demomw.FirstMW(), gin.Recovery()) // adding gin.Recovery() to prevent request chain is called more than one time when panic occurs
-	r.Use(demomw.FirstMW())
 	v1 := r.Group("/v1/api")
 	v1.GET("/test", demomw.SetPanic())
 	r.Run(":8080")
